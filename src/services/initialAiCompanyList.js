4fbd0f8fadc3004db6ee8733351255443e95c8c9
farque65 <fahim.farque@gmail.com>
2024-10-27 16:52:22 -0400

    update ai company list

--- src/services/initialAiCompanyList.js
+++ src/services/initialAiCompanyList.js
@@ -118,7 +118,116 @@ const aiCompanies = [
       "industry": "Drug Discovery AI",
       "tagline": "Decoding biology to radically improve lives",
       "description": "Recursion Pharmaceuticals uses AI and machine learning to accelerate drug discovery and development. Their platform combines automated microscopy, cellular biology, and AI to identify potential new treatments for various diseases."
+    },
+    {
+      "name": "Wolf3d",
+      "industry": "Gaming",
+      "tagline": "The World's Leading Avatar Platform",
+      "description": "Wolf3D specializes in creating personalized 3D avatars for video games and virtual reality through their Ready Player Me platform, which generates avatars from a single selfie."
+    },
+    {
+      "name": "Fliki",
+      "industry": "AI Voice Generation",
+      "tagline": "Turn text into videos with AI voices",
+      "description": "Fliki is an AI-powered text-to-video generator that allows users to create engaging videos quickly and easily, making it ideal for social media content. Users can input text, or even blog posts, and Fliki converts this content into videos with lifelike voiceovers."
+    },
+    {
+      "name": "Meshy.ai",
+      "industry": "3d Model Generation",
+      "tagline": "Create stunning 3d models with ai",
+      "description": "The platform features AI-driven texturing tools, support for multiple art styles, and the ability to create a wide range of models from characters to objects. Meshy AI provides an intuitive interface with options for refining and customizing models."
+    },
+    {
+      "name": "Coda",
+      "industry": "AI Voice Generation",
+      "tagline": "Turn text into videos with AI voices",
+      "description": "Coda is an AI-powered text-to-video generator that allows users to create engaging videos quickly and easily, making it ideal for social media content. Users can input text, or even blog posts, and Coda converts this content into videos with lifelike voiceovers."
+    },
+    {
+      "name": "Blender Foundation",
+      "industry": "3d Model Generation",
+      "tagline": "Create stunning 3d models with ai",
+      "description": "The platform features AI-driven texturing tools, support for multiple art styles, and the ability to create a wide range of models from characters to objects. Blender Foundation provides an intuitive interface with options for refining and customizing models."
+    },
+    {
+      "name": "Hugging Face",
+      "industry": "AI Voice Generation",
+      "tagline": "Turn text into videos with AI voices",
+      "description": "Hugging Face is an AI-powered text-to-video generator that allows users to create engaging videos quickly and easily, making it ideal for social media content. Users can input text, or even blog posts, and Hugging Face converts this content into videos with lifelike voiceovers."
+    },
+    {
+      "name": "Synthesia",
+      "industry": "AI Voice Generation",
+      "tagline": "The #1 AI video communication platform",
+      "description": "Synthesia offers over 400 AI voices in 130+ languages, along with 160+ customizable AI avatars. Synthesia's key features include AI-powered script generation, voice cloning capabilities, and seamless integration with existing content management systems."
+    },
+    {
+      "name": "Wysa",
+      "industry": "Mental Health",
+      "tagline": "Mental Health, redefined",
+      "description": "Wysa is designed as an AI-powered chatbot for mental health conversations, helping people with issues like depression and anxiety. Wysa has conducted over half a billion conversations with more than five million users across 95 countries. The app offers an AI chatbot that provides anonymous, stigma-free support, mood tracking, and self-help exercises designed to improve symptoms of depression and anxiety."
+    },
+    {
+      "name": "Cursor.ai",
+      "industry": "AI Code Editor",
+      "tagline": "Built to make you extraordinary productive, Cursor is the best way to code with AI",
+      "description": "Cursor is an AI-powered code editor that helps developers write code faster and more efficiently. The platform offers features like code completion, error detection, and code suggestions, making it easier for developers to write high-quality code."
+    },
+    {
+      "name": "Exsientia",
+      "industry": "AI Drug Discovery", 
+      "tagline": "Inventors of the worls best AI designed drugs to enter clinical trials",
+      "description": "Exscientia has expanded its AI platform to develop novel therapeutic antibodies through generative AI design and was the first to bring an AI-designed drug candidate to clinical trials in early 2020. The company has also entered into a partnership with the Gates Foundation to develop AI-designed drugs for neglected diseases."
+    },
+    {
+      "name": "BenevolentAI",
+      "industry": "AI Drug Discovery",
+      "tagline": "AI enabled drug discovery",
+      "description": "BenevolentAI utilizes its AI-enabled platform, the Benevolent Platform™. The platform analyzes vast biomedical data, employing machine learning to create a multidimensional representation of human biology across various diseases."
+    },
+    {
+      "name": "Fakespot",
+      "industry": "Fake reviews detection",
+      "tagline": "Use AI to detect fake reviews and scams",
+      "description": "Fakespot is an AI-powered tool designed to help consumers identify fake reviews and counterfeit products when shopping online. Its primary goal is to improve transparency and reliability in e-commerce."
+    },
+    {
+      "name": "Vivint Smart Home AI",
+      "industry": "Smart Home",
+      "tagline": "Smarter Security, Professionally installed",
+      "description": "Vivint Smart Home is a provider of smart home security and automation solutions. They offer a range of products, including home security systems, smart security cameras, smart locks, and smart lighting, all designed to enhance home safety and convenience."
+    },
+    {
+      "name": "Polybee",
+      "industry": "Agriculture",
+      "tagline": "Enabling data driven decision making",
+      "description": "Polybee is an agtech startup that uses AI-powered mini drones for automated pollination and digital phenotyping in indoor agriculture, employing color camera sensors and AI software to vibrate flowers without direct contact."
+    },
+    {
+      "name": "Colossyan",
+      "industry": "AI Video Generation",
+      "tagline": "The AI video creator for your business needs",
+      "description": "he platform features AI presenters, text-to-speech capabilities in over 70 languages, and tools for adding images, music, and animations, making it ideal for employee onboarding and internal communications."
+    },
+    {
+      "name": "Beatoven.ai",
+      "industry": "AI Music Generation",
+      "tagline": "Find the tune that carries your story",
+      "description": "The platform offers text-to-music functionality, allowing users to generate custom tracks based on their ideas, with options for prompt-based customization and adjustments to tempo, instruments, and other musical elements."
+    },
+    {
+      "name": "Dashtoon",
+      "industry": "AI Comic Creation Platform",
+      "tagline": "Bring Your World to Life!",
+      "description": "Users can create consistent designs across panels and explore different genres. The platform utilizes advanced AI technology, including Azure's infrastructure, to generate over 50,000 images daily and offers features like auto-coloring and text bubble insertion."
+    },
+    {
+      "name": "Coffee Robot",
+      "industry": "Enterprise Robotics",
+      "tagline": "The most advanced fully automated café system",
+      "description": "Cafe X Technologies specializes in developing robotic coffee bars that offer fully autonomous, AI-powered coffee service, capable of serving over 100 drinks per hour with minimal daily labor."
     }
+
   ]
 
 
