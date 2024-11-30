b5b76e02ee4a7dcc2ee62980440d5d91a94ae8f2
farque65 <fahim.farque@gmail.com>
2024-11-30 11:47:44 -0500

    featured website

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import CompanyCard from "@/components/CompanyCard";
+import { Badge } from "@/components/ui/badge";
 import FilterModal from "@/components/FilterModal";
 import Footer from "@/components/Footer";
 import { Button } from "@/components/ui/button";
@@ -77,41 +78,44 @@ export default function Home() {
           </Button>
         </div>
 
-        <div className="grid grid-rows-1 mb-4 md:mb-12 lg:mb-20">
+        <div className="grid grid-rows-1 mb-4">
           <div>
             <h2 className="text-2xl font-bold text-foreground mb-4">Featured Companies</h2>
             <p className="text-muted-foreground mb-2">
               These companies are the top picks from our team
             </p>
-            <ul>
-              {featuredCompanies.map((company) => (
-                <li key={company.id} className="flex mt-4">
-                  <div className="flex-grow">
-                    <h2 className="text-gray-900 text-lg title-font font-medium font-bold">{company.name}</h2>
-                    <a className=" text-indigo-500 inline-flex items-center" href={company.website} target="_blank" rel="noopener noreferrer">Learn More
-                      <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                      </svg>
-                    </a>
-                  </div>
-                </li>
-              ))}
-            </ul>
+            <section>
+              <div class="container px-5 py-2 mx-auto">
+                <div class="flex flex-wrap -m-4 text-center">
+                  {featuredCompanies.map((_company: any) => (
+                    <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                      <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                        <h2 class="title-font font-medium text-3xl text-gray-900">{_company.name}</h2>
+                        <div>
+                        {_company?.categories.map((_category: any)=> {
+                          <Badge variant="secondary">{_category}</Badge>
+                        })}
+                        </div>
+                        <p class="leading-relaxed">
+                          <a className=" text-indigo-500 inline-flex items-center" href={_company.website} target="_blank" rel="noopener noreferrer">Learn More
+                            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                              <path d="M5 12h14M12 5l7 7-7 7"></path>
+                            </svg>
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </section>
           </div>
-          {/*
-          <div>
-            <h2 className="text-2xl font-bold text-foreground mb-4">Trending Companies</h2>
-            <p className="text-muted-foreground">
-              Discover the companies that are trending in the AI space
-            </p>
-          </div>
-          */}
         </div>
 
         <h2 className="text-2xl font-bold text-foreground mb-4">All Companies</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {filteredCompanies.map((company) => (
-            <CompanyCard key={company.id} company={company} />
+          {filteredCompanies.map((company: { id: any; name: any; }) => (
+            <CompanyCard key={`${company.id}-${company.name}`} company={company} />
           ))}
         </div>
 
